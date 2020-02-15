f4bf7edcbb2672a3375d1781138e97ae0db3750a
League Student <league@iMac-34.attlocal.net>
2020-02-15 15:50:10 -0800

    Code_4

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,52 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
-	
+
 	public static void main(String[] args) {
+
+		// 1. Create a variable to hold the user's score
+		int score = 0;
+		// 2. Ask the user a question
+		String input = JOptionPane.showInputDialog("How was your day?");
+		String input2 = JOptionPane.showInputDialog("Are you a good person?");
+		String input3 = JOptionPane.showInputDialog("Do you like Java?");
+	
+		// 3. Use an if statement to check if their answer is correct
+		JOptionPane.showMessageDialog(null, "good job!");
+		// 4. if the user's answer was correct, add one to their score
+		if (input.equalsIgnoreCase("bad")) {
+
+		} else if (input.equalsIgnoreCase("good")) {
+			score++;
+
+		}
+
+		if (input2.equalsIgnoreCase("bad")) {
+			score++;
+		}
+
+		else if (input2.equalsIgnoreCase("good")) {
+			JOptionPane.showMessageDialog(null, ("liar"));
+			score--;
+
+		}
+
+		if (input3.equalsIgnoreCase("bad")) {
+			score--;
+			
+			
+		}
 		
-		// 1.  Create a variable to hold the user's score 
+		else if (input3.equalsIgnoreCase("good")) {
+			score++;
+		}
 		
-		// 2.  Ask the user a question 
 		
-		// 3.  Use an if statement to check if their answer is correct
-		
-		// 4.  if the user's answer was correct, add one to their score 
-		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
-		// 6.  After all the questions have been asked, print the user's score 
-		
+		// 5. Create more questions by repeating steps 1, 2, and 3 below.
+
+		// 6. After all the questions have been asked, print the user's score
+		JOptionPane.showMessageDialog(null, "congradulations! your score is "+ score);
 	}
 }
